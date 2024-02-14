ab39a1ca319e7b8301fed30bb5d00c43bdfac3d3
Archaversine <adam.brohl.w@gmail.com>
2024-02-13 22:37:14 -0600

    Better game over screen

--- game_over.gd
+++ game_over.gd
@@ -5,6 +5,6 @@ func _ready():
 	$AnimationPlayer.current_animation = "fade_in"
 	$AnimationPlayer.active = true
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "fade_in":
+		get_tree().change_scene_to_file("res://main_menu.tscn")
